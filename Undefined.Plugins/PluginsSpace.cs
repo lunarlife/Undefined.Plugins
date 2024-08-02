1920c297de7d50ee0bb635835577c95c55bb5360
LunarLife <ivan.andreychenko1502@gmail.com>
2024-08-02 20:28:27 +0200

    Initial commit

---
+++ Undefined.Plugins/PluginsSpace.cs
@@ -0,0 +1,13 @@
+﻿namespace Undefined.Plugins;
+
+public class PluginsSpace<TBase>
+{
+    private readonly Type _pluginBase;
+
+    public PluginsSpace(Type pluginBase, Type mainPlugin)
+    {
+        _pluginBase = pluginBase;
+    }
+
+    public static PluginsSpace<TBase> Create<TMain>() => new(typeof(TBase), typeof(TMain));
+}
